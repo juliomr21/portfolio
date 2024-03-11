9040ec1b2d7894fd036d5ac7ead3ce024d86f4fb
juliomr21 <juliomr21.correo.com>
2024-03-10 22:05:52 -0300

    animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import Aos from 'aos';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -10,4 +10,18 @@ import { FormsModule } from '@angular/forms';
 })
 export class HomeComponent {
 email = 'juliomr21.correo@gmail.com';
+constructor() { }
+
+// @HostListener('scroll', ['$event'])
+// onScroll(event: any) {
+//   // Aquí puedes manejar el evento de desplazamiento
+//   console.log('Se ha desplazado el contenido',window.scrollY);
+// }
+ngOnInit(){
+  Aos.init()
+}
+scroll_home(){
+
+  console.log ('ssss')
+}
 }
