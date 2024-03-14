93ac86abb02908c09d94486208a401825ce4f215
juliomr21 <juliomr21.correo.com>
2024-03-14 14:46:02 -0300

    cv dinamico

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,96 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import Aos from 'aos';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,HttpClientModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 email = 'juliomr21.correo@gmail.com';
-constructor() { }
+portfolio:Portfolio | null = null;
+download = false
+constructor(private http: HttpClient) {
 
-// @HostListener('scroll', ['$event'])
-// onScroll(event: any) {
-//   // Aquí puedes manejar el evento de desplazamiento
-//   console.log('Se ha desplazado el contenido',window.scrollY);
-// }
+ }
 ngOnInit(){
-  Aos.init()
-}
-scroll_home(){
+  Aos.init();
+  this.http.get<Portfolio>('assets/info/pt.json').subscribe(resp => {
+   this.portfolio = resp;
+   this.download = true;
+  });
 
-  console.log ('ssss')
 }
+
+}
+interface Proyecto {
+  tema: string;
+  type: string;
+  empresa: string;
+  dir_site: string;
+  dir_code: string;
+  description: string;
+  image: string;
+}
+
+interface ExperienciaLaboral {
+  centro: string;
+  cargo: string;
+  descripcion: string;
+}
+
+interface Certificado {
+  image: string;
+  empresa: string;
+  titulo: string;
+  link: string;
+  type: 'link' | 'pdf';
+}
+
+interface CursoComplementario {
+  titulo: string;
+  centro: string;
+}
+
+interface skills{
+  image:string;
+  titulo:string
+}
+
+interface Portfolio {
+  personal: {
+      saludo: string;
+      titulo: string;
+      descripcion: string;
+      cv: string;
+      image: string;
+  };
+  skill: {
+      section: string;
+      skills: skills[];
+  };
+  proyectos: {
+      section: string;
+      project: Proyecto[];
+  };
+  experiencia: {
+      section: string;
+      experiencia: ExperienciaLaboral[];
+  };
+  cetificacion: {
+      section: string;
+      certificados: Certificado[];
+  };
+  formacion_principal: {
+      section: string;
+      titulo: string;
+      centro: string;
+  };
+  formacion_complementaria: {
+      section: string;
+      cursos: CursoComplementario[];
+  };
 }
