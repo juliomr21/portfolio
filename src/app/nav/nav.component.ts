f2633cdb46ff64c5b28e37435bdf01a7860a9446
juliomr21 <juliomr21.correo.com>
2024-03-15 14:05:14 -0300

    menu con idiomas

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -9,10 +9,20 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
 menu = false;
+linkPt = ['Habilidades','Projetos','Experiência','Certificações','Formação']
+linkEs = ['Habilidades','Proyectos','Experiencia','Certificaciones','Formación']
+linkEn = ['Skills','Projects','Experience','Certifications','Education']
+linkShow:string[] = [];
+ngOnInit(){
+  if(navigator.language.slice(0,2) == 'es')
+    this.linkShow = this.linkEs;
+    if(navigator.language.slice(0,2) == 'en')
+    this.linkShow = this.linkEn;
+    if(navigator.language.slice(0,2) == 'pt')
+    this.linkShow = this.linkPt;
+}
 closeMenu(){
   this.menu = false
 }
-msg(){
-  console.log('jdjdjd')
-}
+
 }
